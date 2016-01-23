d126817b7d178ccf83647ed9a45fb1bd12c41b19
skellyboy38 <mcauley.robert@live.com>
2016-01-23 17:31:10 -0500

    added functionality

--- content.js
+++ content.js
@@ -11,7 +11,7 @@ chrome.runtime.onMessage.addListener(
   }
 );
 
-var aTags = [];
+var breadthPath = [];
 var first = "";
 var second = "";
 var wikipediaLink = "https://en.wikipedia.org/wiki/";
@@ -22,31 +22,41 @@ function parseValue(value) {
 	second = words[1];
 	var firstLink = wikipediaLink + first;
 	
+	breadthPath.push([first]);
+	
 	visitLink(firstLink);
 }
-
+//======================================================================================================
 function visitLink(link) {
 	window.location.href = link;
-	
-	analyzeLink(link);
-}
 
-function analyzeLink(link) {
-	if(link.search(second) != -1) {
-		finish()
-		return null;
+	var elementToCheck = breadthPath.shift();
+	if(elementToCheck[elementToCheck.length - 1].equalsIgnoreCase(second)) {
+		finish(elementToCheck);
+		return;
 	}
-	var content = document.getElementById("mw-content-text");
-	var pTags = content.getElementsByTagName("p");
-	for each(p in pTags) {
-		var links = p.getElementsByTagName("a");
-		for each(a in links) {
-			var href = a.href;
-			var word = href.substr(6, href.length);
-			aTags.push(a);
+	else {
+		var content = document.getElementById("mw-content-text");
+		var pTags = content.getElementsByTagName("p");
+		for each(p in breadthPath) {
+			var links = p.getElementsByTagName("a");
+			for each(a in links) {
+				var href = a.href;
+				var word = href.substr(6, href.length);	
+				
+				elementToCheck.push(word);
+				breadthPath.push(elementToCheck);
+				
+			}
 		}
+		visitLink(wikipediaLink + breadthPath[0][0]);
 	}
 }
 
-function finish() {
+function finish(array) {
+	var path = "";
+	for each(word in array) {
+		path += (word + "\n");
+	}
+	alert(path);
 }
