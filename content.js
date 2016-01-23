99322aad1d108d9f2fff62d2d627acf5fe55c3dd
skellyboy38 <mcauley.robert@live.com>
2016-01-23 12:04:07 -0500

    modified files to inject jquery

--- content.js
+++ content.js
@@ -1,11 +1,13 @@
+// content.js
 chrome.runtime.onMessage.addListener(
-	function(request, sender, sendResponse) {
-		if(request.message === "clicked_browser_action") {
-			var firstHref = $("a[href^='http']").eq(0).attr("href");
-			
-			console.log(firstHref);
-			
-			chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
-		}
-	}
-};
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+      var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+      console.log(firstHref);
+
+      // This line is new!
+      chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
+    }
+  }
+);
