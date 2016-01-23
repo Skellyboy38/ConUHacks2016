066719397ea04f86ff7448e8314fa046983bfec0
skellyboy38 <mcauley.robert@live.com>
2016-01-23 14:06:39 -0500

    added the onload function and it actually works

--- background.js
+++ background.js
@@ -9,14 +9,16 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   });
 });
 
+// Called when the youtube page is loaded.
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
-  if (changeInfo.status == 'complete') {
+  if (changeInfo.status == 'complete' && tab.active) {
+	// Send a message to the active tab
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		var activeTab = tabs[0];
-		chrome.tabs.sendMessage(activeTab.id, {"message": "page_loaded"});
+		chrome.tabs.sendMessage(activeTab.id, {"message": "loaded_page"});
 	});
   }
-})
+});
 
 // This block is new!
 chrome.runtime.onMessage.addListener(
--- content.js
+++ content.js
@@ -3,18 +3,15 @@ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
       //var firstHref = $("a[href^='http']").eq(0).attr("href");
-		var firstHref = "https://google.com";
+		var firstHref = "https://youtube.com";
       console.log(firstHref);
 
       // This line is new!
       chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
     }
 	else if( request.message === "loaded_page" ) {
-		extractAudio();
+		alert("loaded page");
+		chrome.runtime.sendMessage({"message": "loaded_page"});
 	}
   }
 );
-
-function extractAudio() {
-	alert("The page loaded!");
-}
