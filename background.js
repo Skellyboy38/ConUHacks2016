3524901d823dc8e9b59845d11d42119b2b597f02
skellyboy38 <mcauley.robert@live.com>
2016-01-23 13:33:14 -0500

    added a listener for when the page loads

--- background.js
+++ background.js
@@ -9,6 +9,15 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   });
 });
 
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+  if (changeInfo.status == 'complete') {
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		var activeTab = tabs[0];
+		chrome.tabs.sendMessage(activeTab.id, {"message": "page_loaded"});
+	});
+  }
+})
+
 // This block is new!
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
--- content.js
+++ content.js
@@ -2,12 +2,19 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
-      var firstHref = $("a[href^='http']").eq(0).attr("href");
-
+      //var firstHref = $("a[href^='http']").eq(0).attr("href");
+		var firstHref = "https://google.com";
       console.log(firstHref);
 
       // This line is new!
       chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
     }
+	else if( request.message === "loaded_page" ) {
+		extractAudio();
+	}
   }
 );
+
+function extractAudio() {
+	alert("The page loaded!");
+}
