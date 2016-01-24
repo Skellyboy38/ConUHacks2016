f1f12876014059f290266876e38fff1a68e62909
skellyboy38 <mcauley.robert@live.com>
2016-01-24 00:01:50 -0500

    switched from ajax to javascript

--- content.js
+++ content.js
@@ -24,32 +24,86 @@ function parseValue(value) {
 	
 	breadthPath.push([first]);
 	
-	visitLink(firstLink);
+	visitLink(firstLink, 0);
 }
 //======================================================================================================
-function visitLink(link) {
-	window.location.href = link;
+function visitLink(link, maximum) {
 
+	var links = [];
 	var elementToCheck = breadthPath.shift();	
-	var toCheck = elementToCheck[elementToCheck.length -1].toLowerCase();
+	var toCheck = elementToCheck[0].toLowerCase();
 	
 	if(toCheck == second.toLowerCase()) {
 		finish(elementToCheck);
 		return;
 	}
-	else {
-		var content = document.getElementById("mw-content-text");
-		var aTags = content.getElementsByTagName("a");
-		for(var i = 0; i < aTags.length; i++) {
-			var a = aTags[i];
-			var href = a.getAttribute('href');
-			var word = href.substr(6, href.length);	
-			console.log(word);
-			elementToCheck.push(word);
-			breadthPath.push(elementToCheck);
-		}	
-		visitLink(wikipediaLink + breadthPath[0][0]);
+	else if(maximum >= 10) {
+		finish(elementToCheck);
+		return;
 	}
+	else {
+		/*
+		$.ajaxPrefilter( function (options) {
+			if (options.crossDomain && jQuery.support.cors) {
+				var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
+				options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
+			}
+		});
+	
+		$.get(link, function(response) {
+			var links = $(response).find('#mw-content-text p a.mw-redirect');
+			links.each(function(key, value) {
+				var linkHref = 'https://en.wikipedia.org/' + $(value)[0].href.substring(8);
+				var title = $(value)[0].outerText;
+				title = title.split(' ').join('_');
+				links.push(title);
+			});
+		});*/
+		
+		var response = httpGet(link);
+		var doc = document.createElement('html');
+		doc.innerHTML = response;
+		var pTags = doc.getElementsByTagName("p");
+		
+		var number = 0;
+		for(var i = 0; i < pTags.length; i++) {
+			var aTags = pTags[i].getElementsByTagName("a");
+			for(var j = 0; j < aTags.length; j++) {
+				var word = aTags[j].innerHTML;
+				if(word.charAt(0) != '<') {
+					word = word.split(' ').join('_');
+					links.push(word);	
+					number++;
+				}
+				if(number >= 50) {
+					break;
+				}		
+			}
+		}
+		/*
+		var information = doc.getElementById("mw-content-text");
+		var pTags = information.getElementsByTagName("p");
+		for(var i = 0; i < pTags.length; i++) {
+			console.log(pTags[i].getElementsByTagName("a"));
+		}*/
+		
+		/*
+		for(var i = 0; i < links.length; i++) {
+			var temp = elementToCheck;
+			temp.unshift(links[i]);
+			breadthPath.push(temp);
+		}
+		
+		console.log(breadthPath);
+		visitLink(wikipediaLink + breadthPath[0][0], maximum++);*/
+	}
+}
+
+function httpGet(theUrl) {
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.open( "GET", theUrl, false ); 
+	xmlHttp.send( null );
+	return xmlHttp.responseText;
 }
 
 function finish(array) {
